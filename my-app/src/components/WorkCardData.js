10f3543caeb0e2ae9fc08d1d95764df7aaa49aad
nru707 <nsdanu707@gmail.com>
2024-05-04 11:38:10 +0530

    "commit"

---
+++ my-app/src/components/WorkCardData.js
@@ -0,0 +1,44 @@
+import pro1 from '../Assets/project 1.jpg';
+import pro2 from '../Assets/project 2.jpg';
+import pro3 from '../Assets/project 3.jpg';
+
+const projectCardData = [
+    {
+       imgsrc: pro1,
+       title: 'Ecommerce Website Design',
+       text: 'In Processing, And comming soon...',
+       view: 'url.com',
+       viewsource: 'url.com'
+    },
+    {
+        imgsrc: pro2,
+        title: 'Please wait for new project',
+        text: 'In Processing, And comming soon...',
+        view: 'url.com',
+        viewsource: 'url.com'
+     },
+     {
+        imgsrc: pro3,
+        title: 'Please wait for new project',
+        text: 'In Processing, And comming soon...',
+        view: 'url.com',
+        viewsource: 'url.com'
+     },
+     {
+        imgsrc: pro3,
+        title: 'Please wait for new project',
+        text: 'In Processing, And comming soon...',
+        view: 'url.com',
+        viewsource: 'url.com'
+     },
+     {
+        imgsrc: pro3,
+        title: 'Please wait for new project',
+        text: 'In Processing, And comming soon...',
+        view: 'url.com',
+        viewsource: 'url.com'
+     },
+      
+];
+
+export default projectCardData; 
