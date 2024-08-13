83c4d4d4b6e3c1bfbd28cfcab83a519c44057d23
nru707 <nsdanu707@gmail.com>
2024-08-13 16:16:53 +0530

    commit

--- my-app/src/routes/Contact.js
+++ my-app/src/routes/Contact.js
@@ -1,18 +1,17 @@
-import React, {useRef} from 'react';
+//import React, {useRef} from 'react';
 import Form from '../components/Form';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
 
-
 const Contact = () => {
   return (
     <div>
-       <Navbar />
-       <HeroImg2 heading="CONTACT." text="Lets have a chat" />
-       <Form />
-       <Footer />
-     </div>
+      <Navbar />
+      <HeroImg2 heading="CONTACT." text="Lets have a chat" />
+      <Form />
+      <Footer />
+    </div>
   );
 };
 
